ba4eae3534586ae31fdfd654bb1c3ace80a09d6b
erick260392 <erick260392>
2023-07-24 11:25:01 -0600

    relacionesUnoaUno

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Profile;
 
 class User extends Authenticatable
 {
@@ -41,4 +42,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile(){
+        // $profile = Profile::where('user_id',$this->id)->first();
+
+        // return $profile;
+
+        return $this->hasOne(Profile::class);
+    }
 }
